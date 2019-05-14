a2f20e4916b21f400b218b3252cb10322264c074
joza_oo7 <josipognjan@gmail.com>
2019-05-14 11:00:11 +0200

    Pagination

---
+++ Projekt_RPPP_12/RPPP12/ViewModels/UredajViewModel.cs
@@ -0,0 +1,14 @@
+﻿using RPPP12.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPPP12.ViewModels
+{
+    public class UredajViewModel
+    {
+        public IEnumerable<Uredaj> Uredaji { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
